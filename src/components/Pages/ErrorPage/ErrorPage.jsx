4fb82b3915da7bbbaafd8631f33400111ae63d37
rimakhatun3 <rimamnt46@gmail.com>
2023-05-21 20:38:47 +0600

    update error page

--- src/components/Pages/ErrorPage/ErrorPage.jsx
+++ src/components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import errorImg from '../../../assets/images/error.jpg'
 import { Link } from 'react-router-dom';
+import useTitle from '../../title';
+import { FaArrowLeft } from 'react-icons/fa';
 const ErrorPage = () => {
+    useTitle()
     return (
         <div>
-            <Link to='/'><img className='relative' src={errorImg}  /></Link>
+            <img className='relative' src={errorImg}  />
+            <div className='absolute top-5 left-5'><Link to='/'><button className='btn btn-warning'><FaArrowLeft></FaArrowLeft> Back To Home</button></Link></div>
             {/* <p className='text-center absolute mb-0 '>
               <Link to="/"><button className='btn btn-primary'>Go Back To Home</button> </Link>
             </p> */}
